bb15d45cce4272e05756dcca92071c2d1957726d
ldthinker <253524693@qq.com>
2018-11-23 15:50:15 +0800

    777777777777

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -3,6 +3,6 @@ package com.test;
 public class Hello
 {
 	public static void main(String[] args) {
-		System.out.println("jjjjjjjjjkkkjjjjjjjjjj");
+		System.out.println("jjjjjjjjjkk7777777777j");
 	}
 }
