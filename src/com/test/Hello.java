dcbe9c83da2626c8b784030dae74b8922af434df
ldthinker <253524693@qq.com>
2020-11-07 09:57:32 +0800

    delete files

--- src/com/test/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class Hello
-{
-	public static void main(String[] args) {
-		System.out.println("jjjjjj888888888888887777j");
-		System.out.println("aaaaaaaaa≤‚ ‘aaaa");
-	}
-}
--- src/com/test/World.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class World 
-{
-	public static void main(String[] args)
-	{
-		System.out.println("new world");
-	}
-}
